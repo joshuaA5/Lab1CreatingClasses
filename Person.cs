ef259e084aac5937a618295943a7bb0352132e2c
joshuaA5 <119914122+joshuaA5@users.noreply.github.com>
2023-01-25 19:29:47 -0700

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1Test
+{
+    internal class Person
+    {
+        public int personId;
+        public string firstName;
+        public string lastName;
+        public string favoriteColour;
+        public int age;
+        public bool isWorking;
+
+        public Person(int personId, string firstName, string lastName, string favoriteColour, int age, bool isWorking)
+        {
+            this.personId = personId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.favoriteColour = favoriteColour;
+            this.age = age;
+            this.isWorking = isWorking;
+        }
+
+        public void DisplayPersonInfo()
+        {
+            string firstName = this.firstName;
+            string lastName = this.lastName;
+
+            Console.WriteLine(firstName + " " + lastName);
+        }
+
+        public string personID()
+        {
+            return personId + ":" + " " + firstName + " " + lastName + "'s" + " " + "favorite color is" + " " + favoriteColour;
+        }
+
+        public void changeFavoriteColour()
+        {
+            favoriteColour = "white";         
+        }
+
+        public int getAgeInTenYears()
+        {
+            int age = this.age;
+            return age + 10;
+        }
+
+        public override string ToString()
+        {
+            return "PersonId: " + personId + "\nFirstName: " + firstName + "\nLastName: " + lastName + "\nFavoriteColour: " + favoriteColour + "\nAge: " + age + "\nIsWorking: " + isWorking;     
+        }
+
+
+
+
+    }
+
+        
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1;
+            person1 = new Person(1, "Ian", "Brooks", "Red", 30, true);
+
+            Person person2;
+            person2 = new Person(2, "Gina", "James", "Green", 18, false);
+
+            Person person3;
+            person3 = new Person(3, "Mike", "Briscoe", "Blue", 45, true);
+
+            Person person4;                                  
+            person4 = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+      
+
+            Console.WriteLine(person2.personID());
+            Console.WriteLine(person3.ToString());
+            person1.changeFavoriteColour();
+            Console.WriteLine(person1.personID());
+            Console.WriteLine(person4.firstName + " " + person4.lastName + "'s " + "Age is 10 years is: " + person4.getAgeInTenYears());
+            Console.WriteLine(person3.ToString());
+            Console.WriteLine(person4.ToString());
+            Console.WriteLine(person3.ToString());
+        }
+    }
+}
